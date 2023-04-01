25f452bd2f873f3ab364a8dfc2359cd3f9424fa4
Igor Bińkowski <Igor30522@gmail.com>
2023-04-01 19:46:10 +0200

    Pieczywo

--- app/src/main/java/com/example/lista_zakupy/MainActivity.kt
+++ app/src/main/java/com/example/lista_zakupy/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.lista_zakupy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.chip.Chip
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val bagietka = findViewById<Chip>(R.id.bagietka)
+        bagietka.visibility = View.GONE
+        val croissant = findViewById<Chip>(R.id.croissant)
+        croissant.visibility = View.GONE
+        val chleb =findViewById<Chip>(R.id.chleb)
+        chleb.visibility = View.GONE
     }
 }
