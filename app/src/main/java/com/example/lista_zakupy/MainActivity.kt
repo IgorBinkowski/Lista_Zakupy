2460141683f6f85405d806a6242bae5a3b457993
Igor Bińkowski <Igor30522@gmail.com>
2023-04-01 20:52:26 +0200

    Warzywa

--- app/src/main/java/com/example/lista_zakupy/MainActivity.kt
+++ app/src/main/java/com/example/lista_zakupy/MainActivity.kt
@@ -12,11 +12,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       val bagietka = findViewById<Chip>(R.id.bagietka)
+       val bagietka = findViewById<Chip>(R.id.ziemniak)
         bagietka.visibility = View.GONE
-        val croissant = findViewById<Chip>(R.id.croissant)
+        val croissant = findViewById<Chip>(R.id.salami)
         croissant.visibility = View.GONE
-        val chleb =findViewById<Chip>(R.id.chleb)
+        val chleb =findViewById<Chip>(R.id.kielbasa)
         chleb.visibility = View.GONE
+
+        val szynka = findViewById<Chip>(R.id.szynka)
+        szynka.visibility = View.GONE
+        val salami = findViewById<Chip>(R.id.salami)
+        salami.visibility = View.GONE
+        val kielbasa= findViewById<Chip>(R.id.kielbasa)
+        kielbasa.visibility = View.GONE
+
+        val Ziemniak = findViewById<Chip>(R.id.ziemniak)
+        Ziemniak.visibility = View.GONE
+        val Marchewka = findViewById<Chip>(R.id.marchewka)
+        Marchewka.visibility = View.GONE
+        val Kukurydza= findViewById<Chip>(R.id.kukurydza)
+        Kukurydza.visibility = View.GONE
+        val Salata= findViewById<Chip>(R.id.salata)
+        Salata.visibility = View.GONE
     }
 }
